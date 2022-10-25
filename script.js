305168710b34fad29a0747e5905140915488f537
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-10-25 20:05:03 +0200

    icon homepage

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const links = document.querySelector('.links');
 toggle.addEventListener('click', function () {
     links.classList.toggle('showlinks')
     console.log(links);
-})
+});
 
 const dates = new Date().getFullYear();
 const spanDate = document.querySelector('.date');
