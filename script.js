0c15a5cf7ea68682771fcd6ff11f6a57b8b7931f
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-10-25 19:21:55 +0200

    auto load date

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const toggle=document.querySelector('button');
-const links= document.querySelector('.links');
+const toggle = document.querySelector('button');
+const links = document.querySelector('.links');
 
-toggle.addEventListener('click',function(){
+toggle.addEventListener('click', function () {
     links.classList.toggle('showlinks')
     console.log(links);
 })
+
+const dates = new Date().getFullYear();
+const spanDate = document.querySelector('.date');
+spanDate.innerHTML = dates;
