29f89b89583595c6421f0480a74cd6ca858425a8
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-10-29 19:49:54 +0200

    slider

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// toogle menu
 const toggle = document.querySelector('button');
 const links = document.querySelector('.links');
 
@@ -6,6 +7,42 @@ toggle.addEventListener('click', function () {
     console.log(links);
 });
 
+// slide image 
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevtBtn = document.querySelector('.prevBtn');
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100 }%`;
+
+});
+let counter = 0;
+
+nextBtn.addEventListener('click', function () {
+  counter++;
+  carousel();
+});
+
+prevtBtn.addEventListener('click', function () {
+  counter--;
+  carousel()
+});
+
+
+function carousel() {
+  if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`
+  })
+};
+
+// auto date change for footer
 const dates = new Date().getFullYear();
 const spanDate = document.querySelector('.date');
 spanDate.innerHTML = dates;
